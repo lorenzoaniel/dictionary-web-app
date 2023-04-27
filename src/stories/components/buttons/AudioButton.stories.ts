2b0408657ef7580f0398e530a3ec828fe394ade0
lorenzoaniel <lorenzoaniel@gmail.com>
2023-04-26 19:40:31 -0600

    Story book clean up and new component DarkmodeToggle

--- src/stories/components/buttons/AudioButton.stories.ts
+++ src/stories/components/buttons/AudioButton.stories.ts
@@ -1,7 +1,6 @@
 import AudioButton from "@/components/button/AudioButton";
 import type { Meta, StoryObj } from "@storybook/react";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof AudioButton> = {
 	title: "Components/AudioButton",
 	component: AudioButton,

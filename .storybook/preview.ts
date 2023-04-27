5a2bc9f8a6f1b440959d9e17c7256b47c5d384f5
lorenzoaniel <lorenzoaniel@gmail.com>
2023-04-26 19:07:28 -0600

    added tailwind darkmode support for Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,15 @@
 import type { Preview } from "@storybook/react";
 import "../src/app/styles/globals.css";
 
+export const globalTypes = {
+	darkMode: {
+		defaultValue: false, // Enable dark mode by default on all stories
+	},
+	className: {
+		defaultValue: "dark", // Set your custom dark mode class name
+	},
+};
+
 const preview: Preview = {
 	parameters: {
 		actions: { argTypesRegex: "^on[A-Z].*" },

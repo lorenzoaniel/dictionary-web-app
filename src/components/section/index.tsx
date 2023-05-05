f138563f36dfe8d681f755a5825b1f97fc187dfb
lorenzoaniel <lorenzoaniel@gmail.com>
2023-05-04 20:56:24 -0600

    Section: new variants 'search, audio, ,mainseparator'

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -2,7 +2,7 @@ import { clsx } from "clsx";
 import React from "react";
 
 interface Props {
-	classname: "header" | "search";
+	classname: "header" | "searchbar" | "audio" | "mainseparator";
 	children: JSX.Element | JSX.Element[];
 }
 
@@ -11,6 +11,10 @@ const Section: React.FC<Props> = ({ classname, children }) => {
 		<section
 			className={clsx(classname, "w-full h-fit", {
 				"flex justify-between items-center": classname === "header",
+				"flex justify-between items-center mt-[2.4rem] md:mt-[5.4rem]": classname === "searchbar",
+				"flex justify-between items-center mt-[2.8rem] md:mt-[5rem]": classname === "audio",
+				"flex justify-between items-center mt-[3.4rem] md:mt-[3.4rem]":
+					classname === "mainseparator",
 			})}
 		>
 			{children}
